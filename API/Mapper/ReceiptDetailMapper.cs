45be3b94639012b5686c00fa9cfc374211b1bb20
vanhung38ht <vanhung38ht@gmail.com>
2023-08-16 22:25:11 +0700

    StockReceipt and ReceiptDetail API

---
+++ API/Mapper/ReceiptDetailMapper.cs
@@ -0,0 +1,26 @@
+﻿using API.DTO;
+using API.Models;
+using AutoMapper;
+
+namespace API.Mapper
+{
+    public class ReceiptDetailMapper : Profile
+    {
+        public ReceiptDetailMapper() 
+        {
+            CreateMap<ReceiptDetail, ReceiptDetailDTO>()
+                    .ForMember(des => des.ReceiptDetailId,
+                                act => act.MapFrom(src => src.ReceiptDetailId))
+                    .ForMember(des => des.ReceiptId,
+                                act => act.MapFrom(src => src.ReceiptId))
+                    .ForMember(des => des.ProductId,
+                                act => act.MapFrom(src => src.ProductId))
+                    .ForMember(des => des.EntryPrice,
+                                act => act.MapFrom(src => src.EntryPrice))
+                    .ForMember(des => des.TotalValue,
+                                act => act.MapFrom(src => src.TotalValue))
+                    .ForMember(des => des.EntryUnit,
+                                act => act.MapFrom(src => src.EntryUnit));
+        }
+    }
+}
---
+++ API/Mapper/StockReceiptMapper.cs
@@ -0,0 +1,22 @@
+﻿using API.DTO;
+using API.Models;
+using AutoMapper;
+
+namespace API.Mapper
+{
+    public class StockReceiptMapper : Profile
+    {
+        public StockReceiptMapper() 
+        {
+            CreateMap<StockReceipt, StockReceiptDTO>()
+                        .ForMember(des => des.ReceiptId,
+                                    act => act.MapFrom(src => src.ReceiptId))
+                        .ForMember(des => des.DateReceipt,
+                                    act => act.MapFrom(src => src.DateReceipt))
+                        .ForMember(des => des.WarehouseId,
+                                    act => act.MapFrom(src => src.WarehouseId))
+                        .ForMember(des => des.SupplierId,
+                                    act => act.MapFrom(src => src.SupplierId)); 
+        }
+    }
+}
