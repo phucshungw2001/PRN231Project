5508f4dab0fcd314e6f3522a73aadd297c7a8315
RoyTun <hungnphe150914@fpt.edu.vn>
2023-08-24 20:17:27 +0700

    update register

---
+++ FE_Client/FE_Client/Models/RegisterViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace FE_Client.Models
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [MinLength(6, ErrorMessage = "Password must be at least 6 characters long")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Phone number is required")]
+        [RegularExpression(@"^0\d{9}$", ErrorMessage = "Invalid phone number. Please enter a 10-digit number starting with 0.")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Address is required")]
+        public string Address { get; set; }
+    }
+}
